2780a4b75c77cff8b3b7cdade4941db724e791d9
heeyeon456 <heeyeon@naver.com>
2018-12-16 19:26:57 +0900

    initial commit

---
+++ cProg/c_example/1_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+    char i;
+    for(i=0; i<100; i--) {
+        printf("i = %d\n",  i);
+    }
+    getchar();
+    return 0;
+}
---
+++ cProg/c_example/1_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() 
+{
+    char str[] = "ROBOKING";
+    printf("%s\n",str);
+    printf("%c\n", str[0]);
+    printf("%x\n", str);
+    return 0;
+}
